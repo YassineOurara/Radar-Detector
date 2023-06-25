d435f9ead5f4c14b3fba3ee1848cb8e76d05b3bb
YassineOurara <yassineourara@gmail.com>
2023-06-25 13:38:49 +0100

    keycloak

---
+++ immatriculation-service/src/main/java/org/sid/immatriculationservice/security/KeycloackAdapterConfig.java
@@ -0,0 +1,14 @@
+package org.sid.immatriculationservice.security;
+
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KeycloackAdapterConfig {
+    @Bean
+    KeycloakSpringBootConfigResolver springBootConfigResolver(){
+        return new KeycloakSpringBootConfigResolver();
+    }
+}
